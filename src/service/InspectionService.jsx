efddde73c1b1f9f8933ac516fa3e5aa526b40b55
creativeaseo <smilekimyh@naver.com>
2022-10-24 22:01:35 +0900

    vporta-first

--- src/service/InspectionService.jsx
+++ src/service/InspectionService.jsx
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const INSPECTION_BASE_REST_API_URL = 'https://7e44-14-4-86-86.jp.ngrok.io/api/v1/inspection';
-
-const headers = {
-  'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'Accept': '*/*'
-}
+const INSPECTION_BASE_REST_API_URL = 'https://localhost/api/v1/inspection';
 
 class InspectionService {
 
